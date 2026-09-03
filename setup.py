8ebde4b1145059e86345309f7dc6df9a658aad68
MohammadaliJafari <m.alijafari96@gmail.com>
2026-09-03 17:00:34 +0330

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from os import name
+
+from setuptools import setup, find_packages
+
+setup(
+    name="resume_scorer",
+    author="Mohammadali Jafari",
+    version="0.1",
+    packages=find_packages(),
+)
