0c04add973080cdad05c62957709f62a82508309
MohammadaliJafari <m.alijafari96@gmail.com>
2026-09-03 17:10:56 +0330

    Update custom_exception.py in exceptions

---
+++ exceptions/custom_exception.py
@@ -0,0 +1,26 @@
+import sys
+import traceback
+
+
+class ResumeAnalyserException(Exception):
+    def __init__(self, error_message, error_details):
+        __, __, exc_tb = error_details.exc_info()
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
+        self.lineno = exc_tb.tb_lineno
+        self.error_message = str(error_message)
+        self.traceback_str = "".join(traceback.format_exception(*error_details.exc_info()))
+
+    def __str__(self):
+        return f"""
+            Error in [{self.file_name}] at line [{self.lineno}]
+            Message: [{self.error_message}]
+            Traceback:
+            {self.traceback_str} 
+        """
+
+if __name__ == "__main__":
+    try:
+        a = int("test")
+    except Exception as e:
+        app_exc = ResumeAnalyserException(e, sys)
+        raise  app_exc
